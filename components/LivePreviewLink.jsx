8434ff2ed820e9ff32e56726209cc5b8dc179c73
Ahadul Islam <ahadul.islam@selisegroup.com>
2024-11-07 21:16:31 +0600

    live preview css fix

--- components/LivePreviewLink.jsx
+++ components/LivePreviewLink.jsx
@@ -1,12 +1,12 @@
 export default function (props) {
     return (
-        <div className="live-preview-link ">
+        <div className="border-2 border-secondary my-15 px-6 py-4 rounded-[20px] mt-10 text-lg ">
             Live preview available at
             <br />
-            <div className="link-with-arrow">
+            <div className="mt-4 flex items-center">
                 <img src="/images/link-arrow.svg" alt="arrow icon" />
 
-                <a target="_blank" href={`${props.link}`}>
+                <a className="ml-[8px] text-white" target="_blank" href={`${props.link}`}>
                     {props.link}
                 </a>
             </div>
