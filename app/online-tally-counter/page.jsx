ff23c8f0ebb3dd9676e7c9b929d54286523d2f0c
Ahadul Islam <ahadul.islam@selisegroup.com>
2024-11-07 19:31:53 +0600

    image link fix

--- app/online-tally-counter/page.jsx
+++ app/online-tally-counter/page.jsx
@@ -61,7 +61,7 @@ export default function () {
                     <div className="live-preview-link ">
                         Live preview available at <br />
                         <div className="link-with-arrow">
-                            <img src={`/images/link-arrow.svg`} alt="arrow icon" />
+                            <img src="/images/link-arrow.svg" alt="arrow icon" />
 
                             <a target="_blank" href="https://online-tally-counter.web.app/">
                                 https://online-tally-counter.web.app/
